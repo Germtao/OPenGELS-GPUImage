10ee02ff758ca1eb3b22580e1bcd87053804d193
Mac-01 <TAO@Mac-01deMac-mini.local>
2017-10-27 16:59:20 +0800

    设置 CAEAGLLayer

--- OPenGELS-GPUImage/OPenGELS-GPUImage/ViewController.m
+++ OPenGELS-GPUImage/OPenGELS-GPUImage/ViewController.m
@@ -36,7 +36,7 @@
     [super viewDidLoad];
     
     [self setupOpenGLESContext];
-//    [self setupCAEAGLLayer:self.view.bounds];
+    [self setupCAEAGLLayer:self.view.bounds];
 //    [self clearBuffers];
 //    [self setupBuffers];
 //    [self setupViewPort];
@@ -50,6 +50,17 @@
 - (void)setupOpenGLESContext {
     _eaglContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2]; // openGLES 2.0
     [EAGLContext setCurrentContext:_eaglContext];  // 设置为当前上下文
+}
+
+// STEP2 - 设置 EAGLLayer
+- (void)setupCAEAGLLayer:(CGRect)frame {
+    _eaglLayer = [CAEAGLLayer layer];
+    _eaglLayer.frame = frame;
+    _eaglLayer.backgroundColor = [UIColor cyanColor].CGColor;
+    _eaglLayer.opaque = YES;
+    
+    _eaglLayer.drawableProperties = [NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:NO],kEAGLDrawablePropertyRetainedBacking,kEAGLColorFormatRGBA8,kEAGLDrawablePropertyColorFormat, nil];
+    [self.view.layer addSublayer:_eaglLayer];
 }
 
 
