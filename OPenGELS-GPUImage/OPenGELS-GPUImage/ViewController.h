6b8a5f1a712104578b1e723a0fc59ce9407612c1
Mac-01 <TAO@Mac-01deMac-mini.local>
2017-10-27 16:55:54 +0800

    创建OpenGL上下文，并设为当前上下文

---
+++ OPenGELS-GPUImage/OPenGELS-GPUImage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OPenGELS-GPUImage
+//
+//  Created by TAO on 2017/10/27.
+//  Copyright © 2017年 tTao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OPenGELS-GPUImage/OPenGELS-GPUImage/ViewController.m
@@ -0,0 +1,57 @@
+//
+//  ViewController.m
+//  OPenGELS-GPUImage
+//
+//  Created by TAO on 2017/10/27.
+//  Copyright © 2017年 tTao. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <OpenGLES/ES2/gl.h>
+#import <AVFoundation/AVFoundation.h>
+#import "TXKShaderCompiler.h"
+
+@interface ViewController ()
+{
+    EAGLContext *_eaglContext;
+    CAEAGLLayer *_eaglLayer;
+    
+    GLuint _renderBuffer;
+    GLuint _frameBuffer;
+    
+    // 参数
+    GLuint _positionSlot;
+    GLuint _textureSlot;
+    GLuint _textureCoordSlot;
+    GLuint _colorSlot;
+    
+    TXKShaderCompiler *_shaderCompiler; // 着色器编译器
+}
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self setupOpenGLESContext];
+//    [self setupCAEAGLLayer:self.view.bounds];
+//    [self clearBuffers];
+//    [self setupBuffers];
+//    [self setupViewPort];
+//    [self setupShader];
+}
+
+
+#pragma mark - 设置 Open GL ES
+
+// STEP1 - 创建OpenGL上下文, 并且设置为当前上下文
+- (void)setupOpenGLESContext {
+    _eaglContext = [[EAGLContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES2]; // openGLES 2.0
+    [EAGLContext setCurrentContext:_eaglContext];  // 设置为当前上下文
+}
+
+
+
+@end
