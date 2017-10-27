ca372fe49d17393e779530e606d9d51d1b31337c
Mac-01 <TAO@Mac-01deMac-mini.local>
2017-10-27 17:06:28 +0800

    创建缓冲区

--- OPenGELS-GPUImage/OPenGELS-GPUImage/ViewController.m
+++ OPenGELS-GPUImage/OPenGELS-GPUImage/ViewController.m
@@ -38,7 +38,7 @@
     [self setupOpenGLESContext];
     [self setupCAEAGLLayer:self.view.bounds];
     [self clearBuffers];
-//    [self setupBuffers];
+    [self setupBuffers];
 //    [self setupViewPort];
 //    [self setupShader];
 }
@@ -73,6 +73,31 @@
     if (_frameBuffer) {
         glDeleteFramebuffers(1, &_frameBuffer);
         _frameBuffer = 0;
+    }
+}
+
+// STEP4 - 创建缓冲区, 并绑定
+- (void)setupBuffers {
+    // 帧缓冲区
+    glGenFramebuffers(1, &_frameBuffer);
+    glBindFramebuffer(GL_FRAMEBUFFER, _frameBuffer);
+    // 把颜色渲染缓存 添加到 帧缓存的GL_COLOR_ATTACHMENT0上,就会自动把渲染缓存的内容填充到帧缓存, 在由帧缓存渲染到屏幕
+    glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, _renderBuffer);
+    
+    // 渲染缓冲区
+    glGenRenderbuffers(1, &_renderBuffer);
+    glBindRenderbuffer(GL_RENDERBUFFER, _renderBuffer);
+    // 把渲染缓存绑定到渲染图层上CAEAGLLayer, 并为它分配一个共享内存.
+    // 并且会设置渲染缓存的格式, 和宽度
+    [_eaglContext renderbufferStorage:GL_RENDERBUFFER fromDrawable:_eaglLayer];
+    
+    GLint width = 0;
+    GLint height = 0;
+    glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_WIDTH, &width);
+    glGetRenderbufferParameteriv(GL_RENDERBUFFER, GL_RENDERBUFFER_HEIGHT, &height);
+    //check success
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+        NSLog(@"Failed to make complete framebuffer object: %i", glCheckFramebufferStatus(GL_FRAMEBUFFER));
     }
 }
 
